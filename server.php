4bd230381c042b5ba6b338d66b8f46341c08b634
imtii56 <imtisaladeel9@gmail.com>
2026-09-10 17:58:42 -0500

    updated with dockerfile

--- server.php
+++ server.php
@@ -2,7 +2,8 @@
 //start session
 session_start();
 // build connections
-$conn = mysqli_connect("localhost", "root", "", "web_project");
+$conn = mysqli_connect("sql107.infinityfree.com", "if0_42864112", "IMtoosheikh67", "if0_42864112_web_project");
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
